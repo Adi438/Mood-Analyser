4f2220c1959d6bff97ece4a2d16d1ed6ba68e7fa
Adi438 <mishraaaditya54@gmail.com>
2022-06-02 00:29:10 +0530

    adding refactor code

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MoodAnalser
+namespace MoodAnalyserProject
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\nWelcome to Mood Analyzer");
+            Console.WriteLine("\nEnter Your Current Mood : ");
+            string message = Console.ReadLine();
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message); //Creating a object of MooddAnalyzer class
+            string mood = moodAnalyzer.AnalyzeMood(); // calling method of class with paasing user message as parameter
+            Console.WriteLine($"\nYou are in \"{mood}\" Mood");
         }
     }
 }
+
